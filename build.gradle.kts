d936a5f4e4c5ce2b655fead2e7b3601d9725b0f6
WangZhiYao <393893729@qq.com>
2023-01-04 16:14:43 +0800

    Update: Project build.gradle.kts
    Signed-off-by: WangZhiYao <393893729@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION")
+@file:Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -12,26 +12,5 @@ buildscript {
         classpath(libs.android.tools.build.gradle)
         classpath(libs.kotlin.gradle.plugin)
         classpath(libs.navigation.safe.args.gradle.plugin)
-    }
-}
-
-allprojects {
-    subprojects {
-        project.configurations.all {
-            resolutionStrategy.eachDependency {
-                /*when (requested.group) {
-                    "androidx.activity" -> useVersion("1.6.0")
-                    "androidx.fragment" -> useVersion("1.5.3")
-                    "androidx.annotation" -> useVersion("1.3.0")
-                    "androidx.collection" -> useVersion("1.2.0")
-                    "androidx.core" -> useVersion("1.9.0")
-                    "androidx.customview" -> useVersion("1.1.0")
-                    "androidx.lifecycle" -> useVersion("2.5.1")
-                    "com.google.code.findbugs" -> useVersion("3.0.2")
-                    "org.jetbrains.kotlin" -> useVersion("1.7.20")
-                    "org.jetbrains.kotlinx" -> useVersion("1.6.4")
-                }*/
-            }
-        }
     }
 }
