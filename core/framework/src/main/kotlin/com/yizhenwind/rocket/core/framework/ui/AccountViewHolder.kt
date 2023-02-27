d79c670b7bf2ef6f04ec2c5440dad0334de0b46c
WangZhiYao <393893729@qq.com>
2023-02-28 00:53:52 +0800

    Update: account ui

--- core/framework/src/main/kotlin/com/yizhenwind/rocket/core/framework/ui/AccountViewHolder.kt
+++ core/framework/src/main/kotlin/com/yizhenwind/rocket/core/framework/ui/AccountViewHolder.kt
@@ -1,6 +1,5 @@
 package com.yizhenwind.rocket.core.framework.ui
 
-import com.yizhenwind.rocket.core.common.ext.formatDate
 import com.yizhenwind.rocket.core.framework.databinding.ItemAccountBinding
 import com.yizhenwind.rocket.core.framework.ext.setThrottleClickListener
 import com.yizhenwind.rocket.core.model.Account
@@ -17,7 +16,6 @@ class AccountViewHolder(private val binding: ItemAccountBinding) :
         binding.apply {
             item.apply {
                 tvAccountUsername.text = username
-                tvAccountCreateTime.text = createTime.formatDate()
             }
 
             root.setThrottleClickListener { onItemClickListener?.invoke(item) }
